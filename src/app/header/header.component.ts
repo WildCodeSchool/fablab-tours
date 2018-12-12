0291494c35271ff891cf313126074b73c3013697
maxdhn <maxduchan@gmail.com>
2018-12-12 17:20:00 +0100

    remove unused import

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { HostListener } from'@angular/core';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-	sidebarDisplayed: boolean;
-	constructor() { }
-	
-	ngOnInit() {
-		this.sidebarDisplayed = false;
-	}
-	showSidebar() {
-		this.sidebarDisplayed = !this.sidebarDisplayed;
-	}
-};
+  sidebarDisplayed: boolean;
+  constructor() { }
+
+  ngOnInit() {
+    this.sidebarDisplayed = false;
+  }
+  showSidebar() {
+    this.sidebarDisplayed = !this.sidebarDisplayed;
+  }
+}
