8db6b164b4472104f5f06263baf00b71c3f9a7aa
Loubna Bouam <bouamloubna5@gmail.com>
2018-12-18 17:34:52 +0100

    validation+déclanche l'envoi du mail

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../common/contact.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
 import swal from 'sweetalert2';
 
 
@@ -11,22 +12,21 @@ import swal from 'sweetalert2';
 })
 export class ContactComponent implements OnInit {
   touchForm: FormGroup;
-  submitted = false;
+  // submitted = false;
 
-  constructor(private fb: FormBuilder, private contactService: ContactService) {
-     }
+  constructor(private flashMessages: FlashMessagesService, private fb: FormBuilder, private contactService: ContactService) {
+  }
   ngOnInit() {
     this.touchForm = this.fb.group({
       nom: ['', Validators.required],
-      prenom: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       sujet: ['', Validators.required],
-      message: ['', [Validators.required, Validators.minLength(100)]]
+      message: ['', [Validators.required, Validators.minLength(10)]]
     });
   }
 
   contactForm(form) {
-    this.submitted = true;
+    // this.submitted = true;
     this.contactService.sendMessage(form).subscribe(() => {
       swal('Formulaire de contact', 'Votre message a bien été envoyé', 'success');
     });
