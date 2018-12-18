bad25e12b37adbfbf9c7855a052b2cd2016b14e2
Loubna Bouam <bouamloubna5@gmail.com>
2018-12-18 15:25:12 +0100

    ajout  validation formulaire + css formulaire

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../common/contact.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
 import swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-contact',
@@ -10,37 +10,28 @@ import swal from 'sweetalert2';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  angForm: FormGroup;
-  constructor(private flashMessages: FlashMessagesService, private fb: FormBuilder, private contactService: ContactService) {
-      // this.createForm();
+  touchForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder, private contactService: ContactService) {
      }
-    //  createForm() {
-    //   this.angForm = this.fb.group({
-    //     name: ['', Validators.required],
-    //     email: ['', Validators.required],
-    //     message: ['', Validators    // sendMail(name, email, message) {
-    //   this.contactService.sendEmail(name, email, message).subscribe(success => {
-    //     this.flashMessages.show('You are data we succesfully submitted', { cssClass: 'alert-success', timeout: 3000 });
-    //     console.log(success);
-    //   }, error => {
-    //     this.flashMessages.show('Something went wrong', { cssClass: 'alert-danger', timeout: 3000 });
-    //   });
-    // }.required],
-    //   });
-    // }
-    // sendMail(name, email, message) {
-    //   this.contactService.sendEmail(name, email, message).subscribe(success => {
-    //     this.flashMessages.show('You are data we succesfully submitted', { cssClass: 'alert-success', timeout: 3000 });
-    //     console.log(success);
-    //   }, error => {
-    //     this.flashMessages.show('Something went wrong', { cssClass: 'alert-danger', timeout: 3000 });
-    //   });
-    // }
   ngOnInit() {
-  }
-  contactForm(form) {
-    this.contactService.sendMessage(form).subscribe(() => {
-    swal('Formulaire de contact', 'Votre message a bien été envoyé', 'success');
+    this.touchForm = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      sujet: ['', Validators.required],
+      message: ['', [Validators.required, Validators.minLength(100)]]
     });
-    }
+  }
+
+  contactForm(form) {
+    this.submitted = true;
+    this.contactService.sendMessage(form).subscribe(() => {
+      swal('Formulaire de contact', 'Votre message a bien été envoyé', 'success');
+    });
+  }
 }
+
+
+
