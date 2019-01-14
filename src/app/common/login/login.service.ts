afcaa4e9be55f6bb25ec83380da46734657c776a
Valentin Berruer <valb37@icloud.com>
2019-01-14 16:02:36 +0100

    modif mus login

--- src/app/common/login/login.service.ts
+++ src/app/common/login/login.service.ts
@@ -10,62 +10,32 @@ export class LoginService {
 
   // cretion machine
   sendMachine(body) {
-    return this.http.post('http://localhost:3000/api/machines', body
-      , {
-        headers: new HttpHeaders({
-          'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
-        })
-      });
+    return this.http.post('http://localhost:3000/api/machines', body);
   }
 
   // creation membre
   sendMember(body) {
-    return this.http.post('http://localhost:3000/api/equipe', body
-     , {
-       headers: new HttpHeaders({
-          'Authorization': `BEARER ${localStorage.getItem('access_token')}`,
-       })
-     });
+    return this.http.post('http://localhost:3000/api/equipe', body);
   }
 
   // modification membre
   updateMembre(body, param) {
-    return this.http.put(`http://localhost:3000/modifier/${param}`, body
-    , {
-      headers: new HttpHeaders({
-        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
-      })
-    });
+    return this.http.put(`http://localhost:3000/modifier/${param}`, body);
   }
 
   // suppression  membre
   deleteMembre(param) {
-    return this.http.delete(`http://localhost:3000/supprimer/${param}`
-    , {
-      headers: new HttpHeaders({
-        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
-      })
-    });
+    return this.http.delete(`http://localhost:3000/supprimer/${param}`);
   }
 
   // modification machine
   updateMachine(body, param) {
-    return this.http.put(`http://localhost:3000/modifiermachine/${param}`, body
-    , {
-      headers: new HttpHeaders({
-        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
-      })
-    });
+    return this.http.put(`http://localhost:3000/modifiermachine/${param}`, body);
   }
 
   // suppression machine
   deleteMachine(param) {
-    return this.http.delete(`http://localhost:3000/supprimermachine/${param}`
-    , {
-      headers: new HttpHeaders({
-        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
-      })
-    });
+    return this.http.delete(`http://localhost:3000/supprimermachine/${param}`);
   }
 
 }
