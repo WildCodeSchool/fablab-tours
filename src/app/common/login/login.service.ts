9ba4e462a15305173b2d586f62a4d05df80c0643
musloda <mus_tapha79@outlook.com>
2019-01-15 11:12:23 +0100

    ajout securite equipe et machines

--- src/app/common/login/login.service.ts
+++ src/app/common/login/login.service.ts
@@ -8,7 +8,37 @@ export class LoginService {
 
   constructor(private http: HttpClient) { }
 
-  // cretion machine
+  // creation membre
+  sendMember(body) {
+    return this.http.post('http://localhost:3000/api/equipe', body
+        , {
+      headers: new HttpHeaders({
+        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
+      })
+    });
+  }
+
+  // modification membre
+  updateMembre(body, param) {
+    return this.http.put(`http://localhost:3000/api/equipe/${param}`, body
+        , {
+      headers: new HttpHeaders({
+        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
+      })
+    });
+  }
+
+  // suppression  membre
+  deleteMembre(param) {
+    return this.http.delete(`http://localhost:3000/api/equipe/${param}`
+        , {
+      headers: new HttpHeaders({
+        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
+      })
+    });
+  }
+
+  // creation machine
   sendMachine(body) {
     return this.http.post('http://localhost:3000/api/machines', body
       , {
@@ -18,39 +48,9 @@ export class LoginService {
       });
   }
 
-  // creation membre
-  sendMember(body) {
-    return this.http.post('http://localhost:3000/api/equipe', body
-     , {
-       headers: new HttpHeaders({
-          'Authorization': `BEARER ${localStorage.getItem('access_token')}`,
-       })
-     });
-  }
-
-  // modification membre
-  updateMembre(body, param) {
-    return this.http.put(`http://localhost:3000/modifier/${param}`, body
-    , {
-      headers: new HttpHeaders({
-        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
-      })
-    });
-  }
-
-  // suppression  membre
-  deleteMembre(param) {
-    return this.http.delete(`http://localhost:3000/supprimer/${param}`
-    , {
-      headers: new HttpHeaders({
-        'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
-      })
-    });
-  }
-
   // modification machine
   updateMachine(body, param) {
-    return this.http.put(`http://localhost:3000/modifiermachine/${param}`, body
+    return this.http.put(`http://localhost:3000/api/machines/${param}`, body
     , {
       headers: new HttpHeaders({
         'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
@@ -60,7 +60,7 @@ export class LoginService {
 
   // suppression machine
   deleteMachine(param) {
-    return this.http.delete(`http://localhost:3000/supprimermachine/${param}`
+    return this.http.delete(`http://localhost:3000/api/machines/${param}`
     , {
       headers: new HttpHeaders({
         'Authorization': `BEARER ${sessionStorage.getItem('access_token')}`,
