ac3f310c10036b9b172331b32910ff0bb40f792a
maxdhn <maxduchan@gmail.com>
2019-02-07 17:03:18 +0100

    add quit

--- purge.sh
+++ purge.sh
@@ -8,7 +8,6 @@ get_file_name() {
     echo $( echo "$line" | tr -s ' ' | cut -d ' ' -f9 )
 }
 
-
 purge() {
   local array=$(curl -v -u  ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/$1);
 
@@ -20,14 +19,35 @@ purge() {
       then
         purge "$1$file/"
         echo "Remove directory ftp://funlab.fr/api.funlab.fr/public/$1$file/"
-        curl -u ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/ -Q "RMD api.funlab.fr/public/$1$file/"
+        curl -u ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/ -Q "RMD api.funlab.fr/public/$1$file/;quit"
       elif [[ $type = -* ]]
       then
         echo "Remove file ftp://funlab.fr/api.funlab.fr/public/$1$file"
-        curl -u ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/ -Q "DELE api.funlab.fr/public/$1$file"
+        curl -u ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/ -Q "DELE api.funlab.fr/public/$1$file;quit"
       fi
 
   done <<< "$array"
 }
+
+# purge() {
+#   local array=$(curl -v -u  ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/$1);
+
+#   while read -r line; do
+#       local type=$(get_file_type $line)
+#       local file=$(get_file_name $line)
+
+#       if [[ $type = d* && $file != "public" && $file != "." && $file != ".." ]]
+#       then
+#         purge "$1$file/"
+#         echo "Remove directory ftp://funlab.fr/api.funlab.fr/public/$1$file/"
+#         curl -u ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/ -Q "RMD api.funlab.fr/public/$1$file/"
+#       elif [[ $type = -* ]]
+#       then
+#         echo "Remove file ftp://funlab.fr/api.funlab.fr/public/$1$file"
+#         curl -u ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/ -Q "DELE api.funlab.fr/public/$1$file"
+#       fi
+
+#   done <<< "$array"
+# }
 
 purge ""
