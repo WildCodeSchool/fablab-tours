9135c57b8f7b4a693f9a52638a145c4711c5ef06
maxdhn <maxduchan@gmail.com>
2019-02-07 16:49:07 +0100

    verbose

--- purge.sh
+++ purge.sh
@@ -10,7 +10,7 @@ get_file_name() {
 
 
 purge() {
-  local array=$(curl -u  ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/$1);
+  local array=$(curl -v -u  ${SFTP_USER}:${SFTP_PASSWORD} ftp://funlab.fr/api.funlab.fr/public/$1);
 
   while read -r line; do
       local type=$(get_file_type $line)
